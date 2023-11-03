76e70486aefe212181977ee048acf27548bfbc3d
John Bauer <john.e.b.good@gmail.com>
2023-11-02 23:33:12 -0500

    Editor changes

--- gauge/gauge.js
+++ gauge/gauge.js
@@ -4,7 +4,27 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         this.data       = config.data || "";
         this.dataType   = config.dataType || "msg";
-        this.thumbnail  = config.thumbnail;
+        this.currval        = config.currval || "0";
+        this.animationSpeed = config.animationSpeed || 0;
+        this.angle          = config.angle || "0";
+        this.lineWidth      = config.lineWidth || "0";
+        this.radiusScale    = config.radiusScale || "0";
+        this.ptrLen         = config.ptrLen || "0";
+        this.ptrColor       = config.ptrColor || "0";
+        this.ptrStroke      = config.ptrStroke || "0";
+        this.fontSize       = config.fontSize || "0";
+        this.colorStart     = config.colorStart || "0";
+        this.colorStop      = config.colorStop || "0";
+        this.strokeColor    = config.strokeColor || "0";
+        this.divisionsCbx   = config.divisionsCbx || true;
+        this.divisions      = config.divisions || "0";
+        this.divLength      = config.divLength || "0";
+        this.divColor       = config.divColor || "0";
+        this.divWidth       = config.divWidth || "0";
+        this.subDivisions   = config.subDivisions || "0";
+        this.subLength      = config.subLength || "0";
+        this.subColor       = config.subColor || "0";
+        this.subWidth       = config.subWidth || "0";
         this.active     = (config.active === null || typeof config.active === "undefined") || config.active;
         this.pass       = config.pass;
 
